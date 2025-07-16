e719d4cec75698092e2bb30f1b6c89dedf668575
JC <jcdotnet@hotmail.com>
2025-07-16 16:06:06 +0200

    Added test to create a todo list

---
+++ src/TodoManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+class TodoManager { // Class "App\TodoManager" not found // Done
+
+    public function getTasks(): array { // Call to undefined method App\TodoManager::getTasks() // Done
+
+        // $this->assertCount(0, $tasks); // Argument #2 ($haystack) must be of type Countable|Traversable|array, null given
+        return []; // returning a empty array // Done 
+    }
+
+}
